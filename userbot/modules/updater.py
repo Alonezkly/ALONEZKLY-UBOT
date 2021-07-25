eca27322ba61fa23aa834cb6d767e5620ce6055f
Alonezkly <87901345+Alonezkly@users.noreply.github.com>
2021-07-25 11:10:45 +0700

    Update updater.py

--- userbot/modules/updater.py
+++ userbot/modules/updater.py
@@ -63,7 +63,7 @@ async def deploy(event, repo, ups_rem, ac_br, txt):
                 break
         if heroku_app is None:
             await event.edit(
-                f'{txt}\n`Kredensial Heroku tidak valid untuk deploy 🔥Aʟᴏɴᴇ - Pʀᴏᴊᴇᴄᴛ🔥 dyno.`'
+                f'{txt}\n`Kredensial Heroku tidak valid untuk deploy Aʟᴏɴᴇ - Pʀᴏᴊᴇᴄᴛ dyno.`'
             )
             return repo.__del__()
         await event.edit('`[HEROKU]:'
@@ -91,14 +91,14 @@ async def deploy(event, repo, ups_rem, ac_br, txt):
             await asyncio.sleep(5)
             return await event.delete()
         else:
-            await event.edit("`🔥Aʟᴏɴᴇ - Pʀᴏᴊᴇᴄᴛ🔥 Berhasil Di Deploy!\n" "Restarting, Mohon Tunggu Sebentar.....`")
+            await event.edit("`Aʟᴏɴᴇ - Pʀᴏᴊᴇᴄ Berhasil Di Deploy!\n" "Restarting, Mohon Tunggu Sebentar.....`")
             await asyncio.sleep(15)
             await event.delete()
 
         if BOTLOG:
             await event.client.send_message(
                 BOTLOG_CHATID, "#BOT \n"
-                "`Alonezkly-UBot Berhasil Di Update`")
+                "`Aʟᴏɴᴇ - Pʀᴏᴊᴇᴄᴛ Berhasil Di Update`")
 
     else:
         await event.edit('`[HEROKU]:'
@@ -115,9 +115,9 @@ async def update(event, repo, ups_rem, ac_br):
     except GitCommandError:
         repo.git.reset("--hard", "FETCH_HEAD")
     await update_requirements()
-    await event.edit('**🔥Aʟᴏɴᴇ - Pʀᴏᴊᴇᴄᴛ🔥** `Berhasil Di Update!`')
+    await event.edit('**🔥Aʟᴏɴᴇ - Pʀᴏᴊᴇᴄᴛ🔥.* `Berhasil Di Update!`')
     await asyncio.sleep(1)
-    await event.edit('**🔥Aʟᴏɴᴇ - Pʀᴏᴊᴇᴄᴛ🔥** `Di Restart....`')
+    await event.edit('**🔥Aʟᴏɴᴇ - Pʀᴏᴊᴇᴄᴛ🔥.* `Di Restart....`')
     await asyncio.sleep(1)
     await event.edit('`Mohon Menunggu Beberapa Detik.`')
     await asyncio.sleep(10)
@@ -126,7 +126,7 @@ async def update(event, repo, ups_rem, ac_br):
     if BOTLOG:
         await event.client.send_message(
             BOTLOG_CHATID, "#BOT \n"
-            "**🔥Aʟᴏɴᴇ - Pʀᴏᴊᴇᴄᴛ🔥 Telah Di Perbarui.**")
+            "**🔥Aʟᴏɴᴇ - Pʀᴏᴊᴇᴄᴛ🔥.Telah Di Perbarui.**")
         await asyncio.sleep(100)
         await event.delete()
 
@@ -188,13 +188,13 @@ async def upstream(event):
 
     if changelog == '' and force_update is False:
         await event.edit(
-            f'\n🔥Aʟᴏɴᴇ - Pʀᴏᴊᴇᴄᴛ🔥 Sudah Versi Terbaru\n')
+            f'\n🔥Aʟᴏɴᴇ - Pʀᴏᴊᴇᴄᴛ🔥.Sudah Versi Terbaru\n')
         await asyncio.sleep(15)
         await event.delete()
         return repo.__del__()
 
     if conf is None and force_update is False:
-        changelog_str = f'**Pembaruan Untuk 🔥Aʟᴏɴᴇ - Pʀᴏᴊᴇᴄᴛ🔥 :\n\n💻 Pembaruan Data :**\n`{changelog}`'
+        changelog_str = f'**Pembaruan Untuk 🔥Aʟᴏɴᴇ - Pʀᴏᴊᴇᴄᴛ🔥.:\n\n💻 Pembaruan Data :**\n`{changelog}`'
         if len(changelog_str) > 4096:
             await event.edit("`Changelog Terlalu Besar, Lihat File Untuk Melihatnya.`")
             file = open("output.txt", "w+")
@@ -214,12 +214,12 @@ async def upstream(event):
         await event.edit(
             '`Sinkronisasi Paksa Ke Kode Userbot Stabil Terbaru, Harap Tunggu .....`')
     else:
-        await event.edit('` Proses Update 🔥Aʟᴏɴᴇ - Pʀᴏᴊᴇᴄᴛ🔥, Loading....1%`')
-        await event.edit('` Proses Update 🔥Aʟᴏɴᴇ - Pʀᴏᴊᴇᴄᴛ🔥, Loading....20%`')
-        await event.edit('` Proses Update 🔥Aʟᴏɴᴇ - Pʀᴏᴊᴇᴄᴛ🔥, Loading....35%`')
-        await event.edit('` Proses Update 🔥Aʟᴏɴᴇ - Pʀᴏᴊᴇᴄᴛ🔥, Loading....77%`')
-        await event.edit('` Proses Update 🔥Aʟᴏɴᴇ - Pʀᴏᴊᴇᴄᴛ🔥, Updating...90%`')
-        await event.edit('` Proses Update 🔥Aʟᴏɴᴇ - Pʀᴏᴊᴇᴄᴛ🔥, Mohon Tunggu Sebentar....100%`')
+        await event.edit('` Proses Update 🔥Aʟᴏɴᴇ - Pʀᴏᴊᴇᴄᴛ, Loading....1%`')
+        await event.edit('` Proses Update 🔥Aʟᴏɴᴇ - Pʀᴏᴊᴇᴄᴛ, Loading....20%`')
+        await event.edit('` Proses Update 🔥Aʟᴏɴᴇ - Pʀᴏᴊᴇᴄᴛ, Loading....35%`')
+        await event.edit('` Proses Update 🔥Aʟᴏɴᴇ - Pʀᴏᴊᴇᴄᴛ, Loading....77%`')
+        await event.edit('` Proses Update 🔥Aʟᴏɴᴇ - Pʀᴏᴊᴇᴄᴛ, Updating...90%`')
+        await event.edit('` Proses Update 🔥Aʟᴏɴᴇ - Pʀᴏᴊᴇᴄᴛ, Mohon Tunggu Sebentar....100%`')
     if conf == "now":
         await update(event, repo, ups_rem, ac_br)
         await asyncio.sleep(10)
@@ -234,9 +234,9 @@ async def upstream(event):
 CMD_HELP.update({
     'update':
     "𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.update`"
-    "\n• : Untuk Melihat Pembaruan Terbaru 🔥Aʟᴏɴᴇ - Pʀᴏᴊᴇᴄᴛ🔥."
+    "\n• : Untuk Melihat Pembaruan Terbaru Aʟᴏɴᴇ - Pʀᴏᴊᴇᴄᴛ."
     "\n\n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.update now`"
-    "\n• : Memperbarui 🔥Aʟᴏɴᴇ - Pʀᴏᴊᴇᴄᴛ🔥."
+    "\n• : Memperbarui Aʟᴏɴᴇ - Pʀᴏᴊᴇᴄᴛ."
     "\n\n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.update deploy`"
-    "\n• : Memperbarui 🔥Aʟᴏɴᴇ - Pʀᴏᴊᴇᴄᴛ🔥 Dengan Cara Men-Deploy Ulang."
+    "\n• : Memperbarui Aʟᴏɴᴇ - Pʀᴏᴊᴇᴄᴛ Dengan Cara Men-Deploy Ulang."
 })
