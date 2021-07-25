6dcf0788919afb28ce58d44bf2732d84386cf101
Alonezkly <87901345+Alonezkly@users.noreply.github.com>
2021-07-25 17:48:15 +0700

    Update help.py

--- userbot/modules/help.py
+++ userbot/modules/help.py
@@ -33,7 +33,7 @@ async def help(event):
         for i in CMD_HELP:
             string += "`" + str(i)
             string += "`\t ⊖  "
-        await event.edit("**⚡ AZ-UserBot ⚡**\n\n"
+        await event.edit("**⚡Aʟᴏɴᴇ - Pʀᴏᴊᴇᴄᴛ⚡**\n\n"
                          f"**◉ Bᴏᴛ ᴏꜰ {DEFAULTUSER}**\n**◉ Mᴏᴅᴜʟᴇꜱ : {len(modules)}**\n\n"
                          "**• Mᴀɪɴ Mᴇɴᴜ :**\n"
                          f"◉ {string}◉\n\n")
